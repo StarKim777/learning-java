e01b7db4622fb24dc2eebba2cf99705e16ddaddf
StarKim777 <ppodori3@naver.com>
2024-08-23 16:12:47 +0900

    Variable
    https://how-does-it-work.tistory.com/51

---
+++ java-start/src/variable/Var8.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+
+        //정수
+        byte b = 127; // -128~127
+        short s = 32767; // -32768 ~ 32767
+        int i = 2147483647; // 약 20억
+
+        long il = 2147483647;
+        long li = 2147483649L;
+        long l =  9223372036854775807L; // +대문자 L
+
+        //실수
+        float f = 10.0f; // +소문자 f
+        double d = 10.0;
+    }
+}
